a7d816dddb7d2e79a1c5ac48c3571c289df4f2b6
kim-atkinson <kimfromtoronto@gmail.com>
2023-03-14 15:11:37 -0400

    Added event listener to previous icon.

--- pages.js
+++ pages.js
@@ -1,9 +1,45 @@
+let pageNumber = 0 
+
+
 const nextTag = document.querySelector("footer img.next") //Similar to how you'd pick in CSS
+const previousTag = document.querySelector("footer img.prev")
+const outputTag = document.querySelector("h2")
 
- const outputTag = document.querySelector("h2")
 
+ const next = function () {
+    pageNumber = pageNumber + 1
+
+    if (pageNumber > 4) {
+        pageNumber = 1
+    }
+
+
+    updateSection()
+ }
+
+ const previous = function () {
+    pageNumber = pageNumber - 1
+
+    if (pageNumber < 1) {
+        pageNumber = 4
+    }
+
+    updateSection()
+ }
+
+//  With JS event-based model, order of code doesn't matter. That's when updateSection can be used before it's defined. 
+ const updateSection = function () {
+    outputTag.innerHTML = pageNumber
+ }
 //  Now going to add an event, which states when we do something the following happens. 
 
+
 nextTag.addEventListener("click", function () {
-    outputTag.innerHTML = "it worked!"
+    // Need to run the next function on click
+    next()
+})
+
+previousTag .addEventListener("click", function () {
+    // Need to run the next function on click
+    previous()
 })
